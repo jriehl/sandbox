ccf87154c751a4dbdbb3c02f555d0c25f074508f
Jon Riehl <jriehl@spaceship.com>
2012-06-07 13:45:44 -0500

    Moved C source for numpy_api_user.py into separate file.

--- llvm-py/numpy_api_user.py
+++ llvm-py/numpy_api_user.py
@@ -17,54 +17,14 @@ import numpy as np
 # ______________________________________________________________________
 # Module data
 
-NPY_SRC = '''#include <Python.h>
-#include "ndarrayobject.h"
-
-static PyObject *
-numpyext_testfn(PyObject * self, PyObject * args)
-{
-  PyArrayObject * arr = (PyArrayObject *)NULL;
-  if (!PyArg_ParseTuple(args, "O", &arr)) return NULL;
-  if (!PyArray_Check(arr))
-    {
-      PyErr_SetString(PyExc_ValueError, "Exepected ndarray input.");
-      return NULL;
-    }
-  Py_INCREF(Py_None);
-  return Py_None;
-}
-
-static PyObject *
-numpyext_getndim(PyObject * self, PyObject * args)
-{
-  PyArrayObject * arr = (PyArrayObject *)NULL;
-  if (!PyArg_ParseTuple(args, "O", &arr)) return NULL;
-  if (!PyArray_Check(arr))
-    {
-      PyErr_SetString(PyExc_ValueError, "Exepected ndarray input.");
-      return NULL;
-    }
-  return Py_BuildValue("i", arr->nd);
-}
-
-static PyMethodDef numpyext_methods[] = {
-  {"testfn", numpyext_testfn, METH_VARARGS, NULL},
-  {"getndim", numpyext_getndim, METH_VARARGS, NULL},
-  {NULL, NULL, 0, NULL}
-};
-
-PyMODINIT_FUNC
-initnumpyext(void)
-{
-  import_array();
-  Py_InitModule("numpyext", numpyext_methods);
-}
-'''
+with open('numpy_api_user.c') as cfile:
+    NPY_SRC = cfile.read()
 
 # ______________________________________________________________________
 # Main (demo) routine
 
 def main (*args, **kws):
+    global NPY_SRC
     numpyext = ic.py_module_from_c_source('numpyext', NPY_SRC, '-I',
         os.path.join(np.__path__[0], 'core', 'include', 'numpy'))
     test_array = np.array([1,2,3])
