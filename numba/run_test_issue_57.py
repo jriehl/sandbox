5ca2cc9c42947bea255ff7440d9dfb06ea3709cb
Jon Riehl <jriehl@spaceship.com>
2012-12-14 17:53:21 -0600

    Added more testing code.  Found problem similar to known issue with the pow() test.

--- numba/run_test_issue_57.py
+++ numba/run_test_issue_57.py
@@ -2,4 +2,8 @@
 import logging; logging.getLogger().setLevel(0)
 from numba import *
 import numba.tests.issues.test_issue_57 as ti57
-jit(f4[:,:](i2,f4[:,:]))(ti57.ra_numba)
+c_ra_numba = jit(f4[:,:](i2,f4[:,:]))(ti57.ra_numba)
+c_ra_numpy = jit(f4[:,:](i2,f4[:,:]))(ti57.ra_numpy)
+print c_ra_numpy.lfunc.module
+ti57.benchmark(test_fn = c_ra_numba)
+ti57.benchmark(test_fn = c_ra_numba, control_fn = c_ra_numpy)
