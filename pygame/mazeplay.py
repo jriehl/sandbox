1cbaaf7aba71f8b79a7682202355fd73abe8522a
Jon Riehl <jriehl@spaceship.com>
2017-08-20 18:23:02 -0500

    Egads!  Who put all these CRLF's in here?!

--- pygame/mazeplay.py
+++ pygame/mazeplay.py
@@ -1,10 +1,14 @@
 import array
+import random
 
 from spriteplay import SCREENRECT, TriangleSprite, handle_joystick
 from tileplay import TileMap
 
 import pygame
 from pygame.locals import *
+
+MAZE_WIDTH = 64
+MAZE_HEIGHT = 64
 
 def print_obstructions(grid):
     for index in range(len(grid)):
@@ -19,6 +23,12 @@ def obstructions_from_map(tile_map, source_rect):
     xy_range = ((x, y) for y in y_range for x in x_range)
     return array.array('B', (tile_map.map[y][x] < 17 for (x, y) in xy_range))
 
+def find_empty_tile(tile_map):
+    x_range = range(tile_map.mapSize[0])
+    y_range = range(tile_map.mapSize[1])
+    xy_range = ((x, y) for y in y_range for x in x_range if tile_map.map[y][x] == 17)
+    return random.choice(tuple(xy_range))
+
 def main():
     pygame.init()
     pygame.joystick.init()
@@ -28,8 +38,12 @@ def main():
         joystick.init()
     screen = pygame.display.set_mode(SCREENRECT.size)
     tile_surface = pygame.image.load('data/grid-1.1.bmp').convert()
-    tile_map = TileMap(tile_surface, (32, 32), (128, 64))
+    tile_map = TileMap(tile_surface, (32, 32), (MAZE_WIDTH, MAZE_HEIGHT))
     tile_map.randomMaze()
+    exit_x, exit_y = find_empty_tile(tile_map)
+    exit_rect = Rect(exit_x, exit_y, 1, 1)
+    tile_map.map[exit_y][exit_x] = 18
+    print('Exit:', (exit_x, exit_y))
     entrance_x, entrance_y = tile_map.entrance
     view_rect = Rect(entrance_x - 10, entrance_y - 7, 20, 15).clamp(tile_map.boundRect)
     clock = pygame.time.Clock()
@@ -49,6 +63,21 @@ def main():
         if joystick:
             x_axis, y_axis = handle_joystick(x_axis, y_axis, joystick)
         triangle.move(x_axis, y_axis, grid)
+        if view_rect.contains(exit_rect):
+            exit_pixel_rect = Rect((exit_x - view_rect.x) * 32, (exit_y - view_rect.y) * 32, 32, 32)
+            if exit_pixel_rect.colliderect(triangle.rect):
+                tile_map = TileMap(tile_surface, (32, 32), (MAZE_WIDTH, MAZE_HEIGHT))
+                tile_map.randomMaze()
+                exit_x, exit_y = find_empty_tile(tile_map)
+                exit_rect = Rect(exit_x, exit_y, 1, 1)
+                tile_map.map[exit_y][exit_x] = 18
+                print('Exit:', (exit_x, exit_y))
+                entrance_x, entrance_y = tile_map.entrance
+                view_rect = Rect(entrance_x - 10, entrance_y - 7, 20, 15).clamp(tile_map.boundRect)
+                triangle.rect.x = (entrance_x - view_rect.x) * 32
+                triangle.rect.y = (entrance_y - view_rect.y) * 32
+                grid = obstructions_from_map(tile_map, view_rect)
+                continue
         if not stay_rect.contains(triangle.rect):
             # Scroll
             sprite_x0, sprite_y0 = triangle.rect.topleft
