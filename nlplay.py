73e6d5a542a81e9358efcb81a016c468a21d091a
Jon Riehl <jon.riehl@gmail.com>
2019-07-30 10:35:40 -0500

    Adds deserialization from RDF back into a parse tree.

--- nlplay.py
+++ nlplay.py
@@ -3,23 +3,40 @@ import rdflib
 
 from rdflib.namespace import RDF, RDFS
 
-def walk_tree(graph, branch):
+def tree_to_rdf(graph, branch):
     if isinstance(branch, nltk.Tree):
         result = rdflib.BNode()
         graph.add((result, RDFS.label, rdflib.Literal(branch.label())))
         graph.add((result, RDF.type, RDF.Seq))
         for index, subbranch in enumerate(branch):
-            graph.add((result, getattr(RDF, f'{index + 1}'), walk_tree(graph, subbranch)))
+            graph.add((result, getattr(RDF, f'{index + 1}'), tree_to_rdf(graph, subbranch)))
     else:
         result = rdflib.Literal(branch)
+    return result
+
+def rdf_to_tree(graph, root):
+    if isinstance(root, rdflib.Literal):
+        result = root.toPython()
+    else:
+        label = list(graph[root:RDFS.label])[0].toPython()
+        seq = graph.seq(root)
+        if seq is None:
+            children = []
+        else:
+            children = [rdf_to_tree(graph, child) for child in seq]
+        result = nltk.Tree(label, children)
     return result
 
 def main():
     parser = nltk.parse.CoreNLPParser()
     graph = rdflib.Graph()
     root = list(parser.parse('Welcome to the Ministry of Silly Walks.'.split()))[0]
-    rdf_root = walk_tree(graph, root)
+    rdf_root = tree_to_rdf(graph, root)
     print(graph.serialize(format='turtle').decode())
+    root_2 = rdf_to_tree(graph, rdf_root)
+    assert root == root_2
+    root_2.pretty_print()
+    return graph, root_2
 
 if __name__ == '__main__':
     main()
