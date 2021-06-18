893119217a26dfdc985cef583453aca5e433aa09
Jon Riehl <jon.riehl@gmail.com>
2021-06-17 21:27:46 -0500

    Simplifies AST printing by circumventing temporary output buffer.

--- gobox/cmd/goplay/main.go
+++ gobox/cmd/goplay/main.go
@@ -6,8 +6,6 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
@@ -23,8 +21,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	var buf bytes.Buffer
-	ast.Fprint(&buf, fset, node, nil)
-	fmt.Println(buf.String())
+	err = ast.Print(fset, node)
+	if err != nil {
+		panic(err)
+	}
 	os.Exit(0)
 }
